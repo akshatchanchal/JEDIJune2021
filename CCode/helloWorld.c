25f37b54ac06181ed20397ec6d65dd8e67ff1c04
akshatchanchal <40908800+akshatchanchal@users.noreply.github.com>
2021-06-17 11:51:55 +0530

    Added Greeting function in helloWorld.c

--- CCode/helloWorld.c
+++ CCode/helloWorld.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
+
+void helloWorld();
+void greeting();
+
 int main(){
 	helloWorld();
+	greeting();
 }
-void helloWorld();
+
+void greeting(){
+	printf("Good Morning!");
+}
+
 void helloWorld(){
 	printf("Hello World!");
-	printf("This is C");
 }
